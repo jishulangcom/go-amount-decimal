36333f7beccff573103b02e4e0e622b74531e3fe
wq <815437877@qq.com>
2022-07-31 23:54:27 +0800

    delete redundant code

--- amountdecimal_def.go
+++ amountdecimal_def.go
@@ -8,32 +8,23 @@ type bigRat struct {
 	big.Rat
 }
 
-const bigrat_zero_string = "0/1"
+// const bigrat_zero_string = "0/1"
 
 var bigrat_zero *big.Rat
 var bigint_zero *big.Int
 
-const (
-	int32_negative = '-' // 减号int32
-	int32_spot     = '.' // 点号int32
-	int32_space    = ' ' // 空格int32
-)
-
 // 数据类型
 const (
-	type_ptr     = "ptr"
-	type_string  = "string"
-	type_float32 = "float32"
-	type_float64 = "float64"
+	// type_ptr     = "ptr"
+	type_string = "string"
+	// type_float32 = "float32"
+	// type_float64 = "float64"
 )
 
-var DefaultDecimal = 13 // 默认输出精度
-var amountElementList []int32         // 金额组成元素
+var DefaultDecimal = 13               // 默认输出精度
 var numberTypeMap map[string]struct{} // 数据类型集合
 
 func init() {
-	amountElementList = []int32{'e', '+', '-', '.', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
-
 	list := []string{
 		//"float32", "float64",
 		"int", "int8", "int16", "int32", "int64",
--- amountdecimal_err.go
+++ amountdecimal_err.go
@@ -1,6 +1,5 @@
 package amountdecimal
 
-
 // errorcode
 const (
 	bigrat_setstring_fail = iota
@@ -8,7 +7,6 @@ const (
 	string_setstring_fail
 	to_string_panic
 	amount_not_numeric
-	amount_empty
 	amounts_empty
 	amount_type_wrong
 	amount_type_conversion
@@ -26,12 +24,13 @@ func init() {
 	errCodeMap[string_setstring_fail] = "string setString fail"
 
 	errCodeMap[to_string_panic] = "ToString() panic"
-	errCodeMap[amount_empty] = "amount empty"
+
 	errCodeMap[amounts_empty] = "amounts empty"
-	errCodeMap[amount_not_numeric] = "amount is not of numeric type"
-	errCodeMap[amount_type_wrong] = "amount type is not supported"
+	errCodeMap[amount_not_numeric] = "amount not numeric type"
+	errCodeMap[amount_type_wrong] = "amount type not supported"
 	errCodeMap[amount_type_conversion] = "amount type conversion failed"
 	errCodeMap[amount_divisor_cannot] = "divisor cannot be 0"
+
 	errCodeMap[coin_wrong] = "coin wrong"
 	errCodeMap[coin_type_wrong] = "decimal type wrong"
 }
